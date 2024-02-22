2f0a0bb604eee927af2ef8af28f6e228fd3242cc
Immo <immochotte@gmail.com>
2024-02-22 23:12:24 +0800

    feature: Route Web Dashboard

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::view('/Index', 'Index');
+
+Route::get('/Login', function () {
+    return view('Login');
+});
+
+Route::get('/Pengaduan', function () {
+    return view('Pengaduan');
+});
