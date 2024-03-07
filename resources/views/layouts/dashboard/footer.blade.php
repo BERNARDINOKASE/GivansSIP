4624d3ed322c21a7ab33141a174cfbc3b7e583c0
Bernardino Antonius Kase <reynaldokase.rk@gmail.com>
2024-03-07 09:38:07 +0800

    style: changes dashboard footer

--- resources/views/layouts/dashboard/footer.blade.php
+++ resources/views/layouts/dashboard/footer.blade.php
@@ -1,6 +1,6 @@
 <div class="footer clearfix mb-0 text-muted">
     <div class="float-start">
-        <p>2023 &copy; Mazer</p>
+        <p>2024 &copy; Kelompok Kerja Praktek</p>
     </div>
     <div class="float-end">
         <p>
@@ -8,7 +8,7 @@
             <span class="text-danger"
             ><i class="bi bi-heart-fill icon-mid"></i
             ></span>
-            by <a href="https://saugi.me">Saugi</a>
+            by <a href="https://saugi.me">Kelompok Kerja Praktek</a>
         </p>
     </div>
 </div>
