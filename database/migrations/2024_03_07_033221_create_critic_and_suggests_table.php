6f8474cc6b869a601cbdd5233a96a66ae7c3d612
Bernardino Antonius Kase <reynaldokase.rk@gmail.com>
2024-03-07 12:36:55 +0800

    chore: add id in table critic

--- database/migrations/2024_03_07_033221_create_critic_and_suggests_table.php
+++ database/migrations/2024_03_07_033221_create_critic_and_suggests_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('critic_and_suggests', function (Blueprint $table) {
-            $table->ulid()->primary();
+            $table->ulid('id')->primary();
             $table->enum('category', ['Pengaduan', 'Kritik_dan_Saran'])->required();
             $table->longText('descriptions');
             $table->timestamps();
