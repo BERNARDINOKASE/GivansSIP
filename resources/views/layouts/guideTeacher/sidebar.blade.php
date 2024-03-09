94fed95201342fb583bdf41fc5f894bde17b0c5a
Bernardino Antonius Kase <reynaldokase.rk@gmail.com>
2024-03-09 13:27:00 +0800

    style: changes dashboard url link on guide teacher sidebar

--- resources/views/layouts/guideTeacher/sidebar.blade.php
+++ resources/views/layouts/guideTeacher/sidebar.blade.php
@@ -1,8 +1,8 @@
 <ul class="menu">
     <li class="sidebar-title">Menu</li>
 
-    <li class="sidebar-item {{ Request::routeIs('hrTeacher.index*')?'active':'' }}">
-        <a href="{{route('hrTeacher.index')}}" class="sidebar-link">
+    <li class="sidebar-item {{ Request::routeIs('guideTeacher.index*')?'active':'' }}">
+        <a href="{{route('guideTeacher.index')}}" class="sidebar-link">
         <i class="bi bi-house-door-fill"></i>
         <span>Beranda</span>
         </a>
