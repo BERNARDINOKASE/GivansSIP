b5ed9545cd9b3763f9b9eb602dc0bbb8ec7e7d81
Bernardino Antonius Kase <reynaldokase.rk@gmail.com>
2024-03-09 13:16:13 +0800

    chore: add guideTeacherController

---
+++ app/Http/Controllers/Role/GuideTeacherController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Role;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class GuideTeacherController extends Controller
+{
+    public function index()
+    {
+    }
+}
