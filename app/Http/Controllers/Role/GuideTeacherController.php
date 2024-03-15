40eb7331770a4ed400628bdd893d73e72fb30bc2
Bernardino Antonius Kase <reynaldokase.rk@gmail.com>
2024-03-15 11:11:38 +0800

    feature: add report on waiting and add method new report in guide teacher controller

--- app/Http/Controllers/Role/GuideTeacherController.php
+++ app/Http/Controllers/Role/GuideTeacherController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers\Role;
 
 use App\Http\Controllers\Controller;
+use App\Models\Report;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class GuideTeacherController extends Controller
 {
     public function index()
     {
-        return view('content.guideTeacher.index');
+        $reportWaiting = Report::where('status', 'menunggu')
+            ->where('head_room_teacher_id', Auth::user()->id)->count();
+        return view('content.guideTeacher.index', compact('reportWaiting'));
+    }
+    public function newReport()
+    {
+        $report = Report::where('head_room_teacher_id', Auth::user()->id)->get();
+        dd($report);
     }
 }
