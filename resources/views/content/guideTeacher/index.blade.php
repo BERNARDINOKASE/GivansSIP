f451c4da21db58d5a264c80dabbf6423171917ac
Bernardino Antonius Kase <reynaldokase.rk@gmail.com>
2024-03-15 11:12:17 +0800

    style: add report waiting in guide teacher index view

--- resources/views/content/guideTeacher/index.blade.php
+++ resources/views/content/guideTeacher/index.blade.php
@@ -36,7 +36,7 @@
                             </div>
                             <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
                                 <h6 class="text-muted font-semibold">Pengaduan Belum di Proses</h6>
-                                <h6 class="font-extrabold mb-0"></h6>
+                                <h6 class="font-extrabold mb-0">{{$reportWaiting}}</h6>
                             </div>
                         </div>
                     </div>
