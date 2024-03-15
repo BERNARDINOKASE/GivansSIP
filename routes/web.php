e5aec34f5c034e941ea8c109385221fbb29c4158
Bernardino Antonius Kase <reynaldokase.rk@gmail.com>
2024-03-16 00:31:59 +0800

    chore: change name method on guide teacher routes

--- routes/web.php
+++ routes/web.php
@@ -91,7 +91,7 @@ Route::controller(GuideTeacherController::class)
     ->prefix('/dashboard/guide-teacher/')
     ->group(function () {
         Route::get('/', 'index')->name('guideTeacher.index');
-        Route::get('/new-report', 'newreport')->name('guideTeacher.newreport');
+        Route::get('/new-report', 'report')->name('guideTeacher.report');
     });
 
 
