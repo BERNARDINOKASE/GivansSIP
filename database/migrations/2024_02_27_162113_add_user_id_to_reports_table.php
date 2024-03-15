09d219370dc52660ca684bc40ab09fb0a249a663
Bernardino Antonius Kase <reynaldokase.rk@gmail.com>
2024-03-15 09:42:33 +0800

    chore: add headroom, headmaster, affairs teacher, and guide teacher id to reports table

--- database/migrations/2024_02_27_162113_add_user_id_to_reports_table.php
+++ database/migrations/2024_02_27_162113_add_user_id_to_reports_table.php
@@ -14,6 +14,18 @@ return new class extends Migration
         Schema::table('reports', function (Blueprint $table) {
             $table->foreignUlid('users_id')->required()->after('id')->constrained();
         });
+        Schema::table('reports', function (Blueprint $table) {
+            $table->foreignUlid('head_room_teacher_id')->required()->after('id')->constrained();
+        });
+        Schema::table('reports', function (Blueprint $table) {
+            $table->foreignUlid('guide_teacher_id')->required()->after('id')->constrained();
+        });
+        Schema::table('reports', function (Blueprint $table) {
+            $table->foreignUlid('affairs_teacher_id')->required()->after('id')->constrained();
+        });
+        Schema::table('reports', function (Blueprint $table) {
+            $table->foreignUlid('head_master_id')->required()->after('id')->constrained();
+        });
     }
 
     /**
@@ -22,7 +34,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('reports', function (Blueprint $table) {
-            $table->dropForeign(['users_id']);
+            $table->dropForeign(['users_id, head_room_teacher_id, guide_teacher_id, affairs_teacher_id, head_master_id']);
             $table->dropColumn('users_id');
         });
     }
