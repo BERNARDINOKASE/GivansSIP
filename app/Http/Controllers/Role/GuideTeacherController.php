923de64c1f1d8a0e1a768f048f8db639d94b902a
Bernardino Antonius Kase <reynaldokase.rk@gmail.com>
2024-03-16 13:06:22 +0800

    chore: fix report method and reportEdit method

--- app/Http/Controllers/Role/GuideTeacherController.php
+++ app/Http/Controllers/Role/GuideTeacherController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Role;
 
 use App\Http\Controllers\Controller;
+use App\Models\Offense;
 use App\Models\Report;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -17,8 +19,17 @@ class GuideTeacherController extends Controller
     }
     public function report()
     {
-        $report = Report::where('head_room_teacher_id', Auth::user()->id)->get();
+        $report = Report::where('guide_teacher_id', Auth::user()->id)->get();
         // dd($report);
         return view('content.report.index', compact('report'));
+    }
+
+    public function reportEdit($id)
+    {
+        $report = Report::where('id', $id)->first();
+        $affairsTeacher = User::where('role', 'kesiswaan')->get();
+        // $offense = Offense::all();
+        // dd($report);
+        return view('content.report.edit', compact('report', 'affairsTeacher'));
     }
 }
