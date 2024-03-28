4d1d2e856eb4ad916ba9c8fc6ba8733ecb4a17e8
Bernardino Antonius Kase <reynaldokase.rk@gmail.com>
2024-03-28 11:25:25 +0800

    style: add button ubah password and hapus akun from admin views

--- resources/views/content/admin/users/student.blade.php
+++ resources/views/content/admin/users/student.blade.php
@@ -33,53 +33,49 @@
         <div class="col-md-12 col-12">
             <div class="card">
                 <div class="card-header">
-                <h5 class="card-title">Tabel Siswa</h5>
+                    <h5 class="card-title">Tabel Siswa</h5>
                 </div>
                 <div class="card-body">
-                <table class="table table-striped" id="table1">
-                    <thead>
-                    <tr>
-                        <th>No</th>
-                        <th>NIS</th>
-                        <th>Nama</th>
-                        <th>Email</th>
-                        <th>Jenis Kelamin</th>
-                        <th>No Handphone</th>
-                        <th>Aksi</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach ($student as $item)
+                    <table class="table table-striped" id="table1">
+                        <thead>
                         <tr>
-                            <td>{{$loop->iteration}}</td>
-                            <td>{{$item->second_id}}</td>
-                            <td>{{$item->full_name}}</td>
-                            <td>{{$item->email}}</td>
-                            {{-- <td>{{$item->class_rooms->name}}</td> --}}
-                            {{-- <td>{{$item->gender}}</td> --}}
-                            @if ($item->gender == 'male')
-                                <td class="card-text ms-3">Laki - laki</td>
-                            @else
-                                <td class="card-text ms-3">Perempuan</td>
-                            @endif
-                            <td>{{$item->phone_number}}</td>
-                            <td class="d-lg-inline-flex">
-                                <a href="{{route('hrTeacher.studentShow', $item->id)}}" class="ms-1 btn btn-sm btn-primary" data-bs-toggle="tooltip" data-bs-placement="top" title="Edit">
-                                    {{-- <i class="bi bi-info-circle"></i> --}}
-                                    Edit
-                                </a>
-                                <a href="{{route('hrTeacher.studentShow', $item->id)}}" class="ms-1 btn btn-sm btn-info" data-bs-toggle="tooltip" data-bs-placement="top" title="Lihat detail">
-                                    <i class="bi bi-info-circle"></i>
-                                </a>
-                                <a href="{{route('hrTeacher.studentShow', $item->id)}}" class="ms-1 btn btn-sm btn-danger" data-bs-toggle="tooltip" data-bs-placement="top" title="Hapus">
-                                    {{-- <i class="bi bi-info-circle"></i> --}}
-                                    Hapus
-                                </a>
-                            </td>
+                            <th>No</th>
+                            <th>NIS</th>
+                            <th>Nama</th>
+                            <th>Email</th>
+                            <th>Jenis Kelamin</th>
+                            <th>No Handphone</th>
+                            <th>Aksi</th>
                         </tr>
-                    @endforeach
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                        @foreach ($student as $item)
+                            <tr>
+                                <td>{{$loop->iteration}}</td>
+                                <td>{{$item->second_id}}</td>
+                                <td>{{$item->full_name}}</td>
+                                <td>{{$item->email}}</td>
+                                {{-- <td>{{$item->class_rooms->name}}</td> --}}
+                                {{-- <td>{{$item->gender}}</td> --}}
+                                @if ($item->gender == 'male')
+                                    <td class="card-text ms-3">Laki - laki</td>
+                                @else
+                                    <td class="card-text ms-3">Perempuan</td>
+                                @endif
+                                <td>{{$item->phone_number}}</td>
+                                <td class="d-lg-inline-flex">
+                                    <a href="" class="ms-1 btn btn-sm btn-primary" data-bs-toggle="modal" data-bs-placement="top" title="Ubah Password">
+                                        Ubah password
+                                    </a>
+                                    <a href="" class="ms-1 btn btn-sm btn-danger" data-bs-toggle="modal" data-bs-placement="top" title="Hapus Akun">
+                                        <i class="bi bi-trash"></i>
+                                        Hapus akun
+                                    </a>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
@@ -89,12 +85,4 @@
 @push('script')
     <script src="{{asset('dashboard/')}}/extensions/simple-datatables/umd/simple-datatables.js"></script>
     <script src="{{asset('dashboard/')}}/static/js/pages/simple-datatables.js"></script>
-    <script>
-        document.addEventListener('DOMContentLoaded', function () {
-        var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-        var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-            return new bootstrap.Tooltip(tooltipTriggerEl)
-        })
-    }, false);
-    </script>
 @endpush
